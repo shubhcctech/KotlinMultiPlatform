ece0f8bd6ecab3b3616775910e471c19b9d55f83
shubhcctech <shubham.kurkute@cctech.co.in>
2024-03-28 17:10:01 +0530

    Main Commit

---
+++ shared/src/commonMain/kotlin/Operations.kt
@@ -0,0 +1,25 @@
+class Operations {
+
+
+
+
+        fun add(num1:Int,num2:Int): Int {
+            val sum:Int = num1+num2
+            return sum
+        }
+
+        fun multiply(num1:Int,num2:Int):Int{
+            val sum = num1*num2
+            return sum
+        }
+        fun divide(num1:Int,num2:Int):Int{
+            val sum = num1/num2
+            return sum
+        }
+        fun subtract(num1:Int,num2:Int):Int{
+            val sum = num1-num2
+            return sum
+        }
+
+    }
+
